a3a09bc9c3ba7b5cf692e7c8e1965596441a96d3
Artem <artchzhen@gmail.com>
2023-05-16 16:16:41 +0900

    initial commit development

---
+++ Milestone 22-24/Milestone 22-24/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Milestone 22-24
+//
+//  Created by Артем Чжен on 16/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
